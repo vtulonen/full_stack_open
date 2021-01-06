7bd62ab79d4d90e6ea68d5e9ffc4ac25f4eed6c1
Vili Tulonen <tulonenvili@gmail.com>
2021-01-06 22:58:44 +0200

    1.7 - step 2

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -10,6 +10,7 @@ const Button = ({ text, handleClick }) => (
 );
 
 const StatsItem = ({ name, amount }) => {
+  console.log(amount);
   return (
     <p>
       {name} {amount}
@@ -25,16 +26,46 @@ const App = () => {
   const feedbackTitle = "Give feedback";
   const statsTitle = "Statistics";
 
+  const handleClick = (e) => {
+    const clickedBtn = e.target.innerHTML;
+    switch (clickedBtn) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countSum = () => {
+    return good + neutral + bad;
+  };
+  const countAverage = () => {
+    return (good - bad) / countSum();
+  };
+  const countPositive = () => {
+    return good / countSum();
+  };
+
   return (
     <div>
       <Title text={feedbackTitle} />
-      <Button text={"good"} handleClick={() => setGood(good + 1)} />
-      <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
-      <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
+      <Button text={"good"} handleClick={handleClick} />
+      <Button text={"neutral"} handleClick={handleClick} />
+      <Button text={"bad"} handleClick={handleClick} />
       <Title text={statsTitle} />
       <StatsItem name={"good"} amount={good} />
       <StatsItem name={"neutral"} amount={neutral} />
       <StatsItem name={"bad"} amount={bad} />
+      <StatsItem name={"sum"} amount={countSum()} />
+      <StatsItem name={"average"} amount={countAverage()} />
+      <StatsItem name={"positive"} amount={countPositive()} />
     </div>
   );
 };
