8c86105218584f91a5d32e915d0ad98b225587f2
Vili Tulonen <tulonenvili@gmail.com>
2021-01-07 20:40:21 +0200

    2.1 - step 6

---
+++ osa_2/kurssitiedot/src/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Course = ({ course }) => {
+  return (
+    <div className="course">
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course }) => {
+  return (
+    <div className="content">
+      {course.parts.map((item) => (
+        <Part key={item.id} name={item.name} exercises={item.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p className="part">
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Test",
+        exercises: 5,
+        id: 4,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
