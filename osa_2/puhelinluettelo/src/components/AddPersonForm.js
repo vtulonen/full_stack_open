e3b375fa2fa2b16aa5e4a830412cc9335f9a7d14
Vili Tulonen <tulonenvili@gmail.com>
2021-01-08 15:24:02 +0200

    2.7 - step 2

--- osa_2/puhelinluettelo/src/components/AddPersonForm.js
+++ osa_2/puhelinluettelo/src/components/AddPersonForm.js
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const AddPersonForm = ({newName, setNewName, persons, setPersons}) => {
+const AddPersonForm = ({ newName, setNewName, persons, setPersons }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
     };
-  
-    setPersons([...persons, newPerson]);
+
+    // Palauttaa undefined jos mikään person.name ei vastaa newNamea
+    persons.find((person) => person.name === newName) === undefined
+      ? setPersons([...persons, newPerson])
+      : alert(`${newName} is already in the phonebook`);
   };
 
   const handleNameChange = (e) => {
-    console.log(e.target.value);
     setNewName(e.target.value);
   };
 
   return (
-      <form onSubmit={onSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-  )
-}
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
 
-export default AddPersonForm
-
-
-
+export default AddPersonForm;
