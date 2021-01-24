ad375928514be7f14d5c86d08889cba189109faa
Vili Tulonen <tulonenvili@gmail.com>
2021-01-24 14:48:33 +0200

    4.5

--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -5,16 +5,22 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   const reducer = (accumulator, currentValue) => {
     return accumulator + currentValue
-  } 
- 
+  }
+
   const likesArray = blogs.map((item) => {
     return item.likes
   })
 
-  return likesArray.reduce(reducer, 0);
+  return likesArray.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = Math.max(...blogs.map((item) => item.likes)) // Amount of likes for the most liked blog
+  return blogs.find((item) => item.likes === mostLikes) // Find and return the object with most likes
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
