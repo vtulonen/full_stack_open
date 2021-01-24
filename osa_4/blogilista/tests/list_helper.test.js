59c503691c585170abca82b242a4dd35d18052ac
Vili Tulonen <tulonenvili@gmail.com>
2021-01-24 16:16:08 +0200

    add lodash + refactored favorite blog test function

--- osa_4/blogilista/tests/list_helper.test.js
+++ osa_4/blogilista/tests/list_helper.test.js
@@ -91,14 +91,11 @@ describe('total likes', () => {
 })
 
 describe('favorite blog', () => {
-  test('empty bloglist returns 0', () => {
+  test('is the one with most likes', () => {
     expect(listHelper.favoriteBlog(blogs)).toEqual({
-      _id: '5a422b3a1b54a676234d17f9',
       title: 'Canonical string reduction',
       author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
       likes: 12,
-      __v: 0,
     })
   })
 })
--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -16,7 +18,8 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   const mostLikes = Math.max(...blogs.map((item) => item.likes)) // Amount of likes for the most liked blog
-  return blogs.find((item) => item.likes === mostLikes) // Find and return the object with most likes
+  const favoriteBlog = blogs.find((item) => item.likes === mostLikes) // Find and return the object with most likes
+  return _.pick(favoriteBlog, ['title', 'author', 'likes'])
 }
 
 module.exports = {
