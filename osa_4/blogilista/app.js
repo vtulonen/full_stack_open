5eb9042613dff5863f1072f83e0fa453e5e9aeff
Vili Tulonen <tulonenvili@gmail.com>
2021-01-24 12:26:06 +0200

    4.2

---
+++ osa_4/blogilista/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+
+module.exports = app
