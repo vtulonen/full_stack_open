a41c4c5d58c0cacfeeb66c6b951472710f98d1c9
Vili Tulonen <tulonenvili@gmail.com>
2021-01-24 14:27:02 +0200

    4.4

--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => {
+    return accumulator + currentValue
+  } 
+ 
+  const likesArray = blogs.map((item) => {
+    return item.likes
+  })
+
+  return likesArray.reduce(reducer, 0);
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
