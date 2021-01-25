a376de578dd7f7965af1a29bfb69e3585b3c4584
Vili Tulonen <tulonenvili@gmail.com>
2021-01-25 19:29:01 +0200

    4.9

--- osa_4/blogilista/tests/blog_api.test.js
+++ osa_4/blogilista/tests/blog_api.test.js
@@ -2,33 +2,14 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
-
+const helper = require('../tests/test_helper')
 const Blog = require('../models/blog')
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url:
-      'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -42,6 +23,13 @@ test('blogs are returned as json', async () => {
 test('two blogs in db', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(2)
+})
+
+test('returned blogs have a key id, not _id', async () => {
+  const response = await api.get('/api/blogs')
+  console.log(response.body[0].id)
+
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
