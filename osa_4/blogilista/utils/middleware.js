d6b20b3c3618a68d548cd7ab3b0edd3a909bbc41
Vili Tulonen <tulonenvili@gmail.com>
2021-01-29 14:38:41 +0200

    4.20

--- osa_4/blogilista/utils/middleware.js
+++ osa_4/blogilista/utils/middleware.js
@@ -17,14 +17,26 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.name)
 
   if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 }
