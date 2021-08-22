5033f5cdd1cc096aa8d3726697c4b63352def21d
Vili Tulonen <tulonenvili@gmail.com>
2021-08-22 12:02:09 +0300

    6.20 connected filter

--- osa_6/redux-anecdotes/src/components/Filter.js
+++ osa_6/redux-anecdotes/src/components/Filter.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import { setFilter } from '../reducers/filterReducer'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Filter = () => {
-  const dispatch = useDispatch()
-
+const Filter = (props) => {
   const handleChange = (event) => {
     const filter = event.target.value
-    dispatch(setFilter(filter))
-  }
-  const style = {
-    marginBottom: 10,
+    props.setFilter(filter)
   }
 
   return (
-    <div style={style}>
+    <div style={{marginBottom: 10}}>
       filter <input onChange={handleChange} />
     </div>
   )
 }
 
-export default Filter
+export default connect(
+  null,
+  { setFilter }
+)(Filter)
+
